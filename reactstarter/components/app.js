6b75474b4d777b0ee14435d63d869fef69a6733b
BIT Student <classroom@bgit.rs>
2017-11-13 17:26:58 +0100

    created blog project

---
+++ reactstarter/components/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>Belgrade Institute of Technology</h4>
+                <h1>Hello from react!</h1>
+            </div>
+            
+        );
+    }
+}
+
+export default App;
